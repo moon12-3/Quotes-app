ee277e3fff803fcf40bbae156aab8290d2f19076
chaeyoung moon <cymoon05@naver.com>
2022-04-27 23:30:55 +0900

    명언 공유, 출처 검색 기능 추가

--- app/src/main/java/com/example/todayquote/QuoteAdapter.kt
+++ app/src/main/java/com/example/todayquote/QuoteAdapter.kt
@@ -1,8 +1,11 @@
 package com.example.todayquote
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -16,11 +19,38 @@ class QuoteAdapter(private val dataList : List<Quote>) : RecyclerView.Adapter<Qu
         val quoteText = view.findViewById<TextView>(R.id.quote_text)
         val quoteFrom = view.findViewById<TextView>(R.id.quote_from)
 
+        val shareBtn = view.findViewById<Button>(R.id.quote_share_btn)
+        val fromSearchBtn = view.findViewById<Button>(R.id.quote_from_search_btn)
+
+        init {
+            shareBtn.setOnClickListener {
+                val intent = Intent(Intent.ACTION_SEND)
+
+                intent.putExtra(Intent.EXTRA_TITLE, "힘이 되는 명언")
+                intent.putExtra(Intent.EXTRA_SUBJECT, "힘이 되는 명언")
+                intent.putExtra(Intent.EXTRA_TEXT, "${quote.text}\n출처 : ${quote.from}")
+                intent.type = "text/plain"
+                
+                val chooser = Intent.createChooser(intent, "명언 공유")
+
+                it.context.startActivity(chooser)
+            }
+
+            fromSearchBtn.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=${quote.from}"))
+                it.context.startActivity(intent)
+            }
+        }
+
         fun bind(q : Quote) {
             this.quote = q
 
             quoteText.text = quote.text
             quoteFrom.text = quote.from
+
+            if(quote.from.isBlank()) {
+                fromSearchBtn.visibility = View.GONE
+            }
         }
     }
 
